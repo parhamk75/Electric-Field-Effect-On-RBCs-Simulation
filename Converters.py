3b8638c9f7da3b1e1a05d1f9bbdf639a1964f16e
Parham <parhamk75@hotmail.com>
2020-01-26 00:14:35 +0330

    main file and the converter module added to project

---
+++ Converters.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 25 20:01:37 2020
+
+@author: parha
+"""
+
+def m3_to_lit(m3= 0):
+    """
+        Converts 'm^3' to 'lit'
+        -m3
+    """
+    return m3* 1e3
+
+
+def mol_m3_to_mol_lit(mol_m3= 0):
+    """
+        Converts 'mol/m^3' to 'mol/lit'
+        - mol/m^3
+    """
+    return mol_m3* 1e-3
---
+++ main.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 25 18:09:24 2020
+
+@author: parha
+"""
+
+#%% Import
+
+import MiscellaneousFunctions as misc
