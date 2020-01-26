e8874f2668a3ac7de07e77e3dd9af80031ece50c
Parham <parhamk75@hotmail.com>
2020-01-27 00:51:53 +0330

    Ca2+ channel model apparently completed

--- MiscellaneousFunctions.py
+++ MiscellaneousFunctions.py
@@ -14,7 +14,7 @@ from Converters import mol_m3_to_mol_lit
 
 a_d         = prms.A/(prms.F* prms.z_Ca* prms.Vol)
 
-def Ca_Dens_Calc(D_Ca_i_prev= 0, I_Ca_curr= 0):
+def Ca_Dens_Calc_R(D_Ca_i_prev= 0, I_Ca_curr= 0):
     """
         Calculate the current intracellular Density of Ca2+ by the relation below:
             [Ca2+_i][n] = [Ca2+_i][n-1] + a_d* Ts* I_Ca[n]
@@ -37,7 +37,7 @@ def Ca_Dens_Calc(D_Ca_i_prev= 0, I_Ca_curr= 0):
 ZF_RT   = 2* 37.45318352                        # (Zp*F)/(R*T)
 a_i     = (4* 0.0777* 92.16* 1e8) / (2563.2) 
 
-def Trans_Mem_Current_Calc(V_m= 0, m= 0, D_Ca_i= 0):
+def Trans_Mem_Current_Calc_R(V_m= 0, m= 0, D_Ca_i= 0):
     """
         Calculates the Transmembrane Ca2+ Current due to GHK Model
         
@@ -59,14 +59,77 @@ def Trans_Mem_Current_Calc(V_m= 0, m= 0, D_Ca_i= 0):
         tmp_var = np.exp(ZF_RT* V_m)
         return (a_i* V_m* m) * ( (prms.D_Ca_o - (D_Ca_i* tmp_var) ) / (1 - tmp_var) )
     
+#%% m Calculating Function and its essential functions
 
+def a_m_Calc_R(V_m= 0):
+    """
+        Calculates the alpha
+        
+        - V_m:
+            Vm[n]
+            in 'V'
+        - returns:
+            alpha[n]
+            in ????
+            #TODO: What is the unit of alpha?
+    """
+    return 8.5 / (1 + np.exp( ((1e3* V_m)-8) / -12.5 ) ) 
+       
+def b_m_Calc_R(V_m= 0):
+    """
+        Calculates the beta
+        
+        - V_m:
+            Vm[n]
+            in 'V'
+        - returns:
+            beta[n]
+            in ????
+            #TODO: What is the unit of beta?
+    """
+    return 35 / (1 + np.exp( ((1e3* V_m)+74) / 14.5 ) )
+    
+def tau_Calc_R(a_m= 0, b_m= 0):
+    """
+        Calculates the beta
+        
+        - a_m:
+            alpha[n]
+            in ????
+        - b_m:
+            beta[n]
+            in ????
+        - returns:
+            tau[n]
+            in ????
+            #TODO: What is the unit of tau?
+    """
+    return 1 / (a_m + b_m)
+
+c_m = 1e3 / ( (4* 0.0777* 92.16* 1e8) / (2563.2) )
+
+def m_Inf_Calc_R(V_m= 0, D_Ca_i= 0, a_m= 0, b_m= 0):
+    """
+    """
+    tmp_var_1 = np.exp(ZF_RT* V_m)
+    tmp_var_2 = (1 - tmp_var_1) / (prms.D_Ca_o - (D_Ca_i* tmp_var_1) )
+    tmp_var_3 = ( V_m - 0.135 ) / V_m
+    tmp_var_4 = a_m / ( a_m + b_m )
+    return c_m* tmp_var_1* tmp_var_2* tmp_var_3* tmp_var_4
+
+def m_Calc_R(tau_curr= 0, m_prev= 0, m_inf_curr= 0):
+    """
+    """
+    
+    return ((1 - (prms.Ts/tau_curr))* m_prev) + ((prms.Ts/tau_curr)* m_inf_curr)
+    
 #%% Test
 
 if __name__=='__main__':
-    # --> Ca_Dens_Calc()
-    print( Ca_Dens_Calc(prms.D_Ca_i_0, 10e-2) )
+    # --> Ca_Dens_Calc_R()
+    print( Ca_Dens_Calc_R(prms.D_Ca_i_0, 10e-2) )
     
-    # --> Trans_Mem_Current_Calc()
+    # --> Trans_Mem_Current_Calc_R()
     import matplotlib.pyplot as plt
     Im = []
     m = np.linspace(0, 1, 100)
@@ -74,7 +137,7 @@ if __name__=='__main__':
     
     for j in Vm:
         for i in m:
-            Im.append( Trans_Mem_Current_Calc(j, i, prms.D_Ca_i_0*10) )
+            Im.append( Trans_Mem_Current_Calc_R(j, i, prms.D_Ca_i_0*10) )
             
         plt.plot(m,Im,'.-')
         Im = []
