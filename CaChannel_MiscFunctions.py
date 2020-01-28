4c17e37834dfd013c49b2e9b31cdde7cb0cb114f
Parham <parhamk75@hotmail.com>
2020-01-28 09:15:52 +0330

    Ca Pump model added + current names modified

--- CaChannel_MiscFunctions.py
+++ CaChannel_MiscFunctions.py
@@ -12,9 +12,7 @@ from Converters import mol_m3_to_mol_lit
 
 #%% Calcium Intracellular Density Calculating Function
 
-a_d         = prms.A/(prms.F* prms.z_Ca* prms.Vol)
-
-def Ca_Dens_Calc_R(D_Ca_i_prev= 0, I_Ca_curr= 0):
+def Ca_Dens_Calc(D_Ca_i_prev= 0, I_Ca_curr= 0):
     """
         Calculate the current intracellular Density of Ca2+ by the relation below:
             [Ca2+_i][n] = [Ca2+_i][n-1] + a_d* Ts* I_Ca[n]
@@ -29,17 +27,15 @@ def Ca_Dens_Calc_R(D_Ca_i_prev= 0, I_Ca_curr= 0):
             [Ca2+_i][n]
             in 'mol/m3'
     """
+    a_d     = prms.A/(prms.F* prms.z_Ca* prms.Vol)
     
     return D_Ca_i_prev + (a_d* prms.Ts* I_Ca_curr)
 
-#%% Transmembrane Ca2+ Current Calculating Function
+#%% Transmembrane Ca2+ Current through Ca2+ Channel Calculating Function
 
-ZF_RT   = 2* 37.45318352                        # (Zp*F)/(R*T)
-a_i     = (4* 0.0777* 92.16* 1e8) / (2563.2) 
-
-def Trans_Mem_Current_Calc_R(V_m= 0, m= 0, D_Ca_i= 0):
+def Ca_Channel_Current_Calc(V_m= 0, m= 0, D_Ca_i= 0):
     """
-        Calculates the Transmembrane Ca2+ Current due to GHK Model
+        Calculates the Transmembrane Ca2+ Current through the Ca2+ Channels due to GHK Model
         
         - V_m:
             Vm[n]
@@ -50,9 +46,13 @@ def Trans_Mem_Current_Calc_R(V_m= 0, m= 0, D_Ca_i= 0):
             [Ca2+_i][n]
             in 'mol/lit'
         - returns:
-            Im[n]
+            Ich_Ca[n]
             in 'pA/cm^2'
     """
+    
+    ZF_RT   = 2* 37.45318352                        # (Zp*F)/(R*T)
+    a_i     = (4* 0.0777* 92.16* 1e8) / (2563.2) 
+
     if V_m == 0:
         return 0
     else:
@@ -61,7 +61,7 @@ def Trans_Mem_Current_Calc_R(V_m= 0, m= 0, D_Ca_i= 0):
     
 #%% m Calculating Function and its essential functions
 
-def a_m_Calc_R(V_m= 0):
+def a_m_Calc(V_m= 0):
     """
         Calculates the alpha
         
@@ -70,12 +70,11 @@ def a_m_Calc_R(V_m= 0):
             in 'V'
         - returns:
             alpha[n]
-            in ????
-            #TODO: What is the unit of alpha?
+            in '1/ms'
     """
     return 8.5 / (1 + np.exp( ((1e3* V_m)-8) / -12.5 ) ) 
        
-def b_m_Calc_R(V_m= 0):
+def b_m_Calc(V_m= 0):
     """
         Calculates the beta
         
@@ -84,12 +83,11 @@ def b_m_Calc_R(V_m= 0):
             in 'V'
         - returns:
             beta[n]
-            in ????
-            #TODO: What is the unit of beta?
+            in '1/ms'
     """
     return 35 / (1 + np.exp( ((1e3* V_m)+74) / 14.5 ) )
     
-def tau_Calc_R(a_m= 0, b_m= 0):
+def tau_Calc(a_m= 0, b_m= 0):
     """
         Calculates the beta
         
@@ -101,23 +99,24 @@ def tau_Calc_R(a_m= 0, b_m= 0):
             in ????
         - returns:
             tau[n]
-            in ????
-            #TODO: What is the unit of tau?
+            in 'ms'
     """
     return 1 / (a_m + b_m)
 
-c_m = 1e3 / ( (4* 0.0777* 92.16* 1e8) / (2563.2) )
-
-def m_Inf_Calc_R(V_m= 0, D_Ca_i= 0, a_m= 0, b_m= 0):
+def m_Inf_Calc(V_m= 0, D_Ca_i= 0, a_m= 0, b_m= 0):
     """
     """
+    c_m = 1e3 / ( (4* 0.0777* 92.16* 1e8) / (2563.2) )
+    ZF_RT   = 2* 37.45318352                        # (Zp*F)/(R*T)
+    
     tmp_var_1 = np.exp(ZF_RT* V_m)
     tmp_var_2 = (1 - tmp_var_1) / (prms.D_Ca_o - (D_Ca_i* tmp_var_1) )
     tmp_var_3 = ( V_m - 0.135 ) / V_m
     tmp_var_4 = a_m / ( a_m + b_m )
+    
     return c_m* tmp_var_1* tmp_var_2* tmp_var_3* tmp_var_4
 
-def m_Calc_R(tau_curr= 0, m_prev= 0, m_inf_curr= 0):
+def m_Calc(tau_curr= 0, m_prev= 0, m_inf_curr= 0):
     """
     """
     
@@ -126,21 +125,21 @@ def m_Calc_R(tau_curr= 0, m_prev= 0, m_inf_curr= 0):
 #%% Test
 
 if __name__=='__main__':
-    # --> Ca_Dens_Calc_R()
-    print( Ca_Dens_Calc_R(prms.D_Ca_i_0, 10e-2) )
+    # --> Ca_Dens_Calc()
+    print( Ca_Dens_Calc(prms.D_Ca_i_0, 10e-2) )
     
-    # --> Trans_Mem_Current_Calc_R()
+    # --> Ca_Channel_Current_Calc()
     import matplotlib.pyplot as plt
-    Im = []
+    Ich_Ca = []
     m = np.linspace(0, 1, 100)
     Vm = np.linspace(-60e-3, 250e-3, 6)
     
     for j in Vm:
         for i in m:
-            Im.append( Trans_Mem_Current_Calc_R(j, i, prms.D_Ca_i_0*10) )
+            Ich_Ca.append( Ca_Channel_Current_Calc(j, i, prms.D_Ca_i_0*10) )
             
-        plt.plot(m,Im,'.-')
-        Im = []
+        plt.plot(m,Ich_Ca,'.-')
+        Ich_Ca = []
     
     plt.legend(Vm, loc=[0,0])
     plt.hlines(0,-0.1,1.1, colors='fuchsia')
